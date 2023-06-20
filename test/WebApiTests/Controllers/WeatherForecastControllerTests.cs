75f11e0a0aa0bccfcbcfacfac88c4c8a451be395
fndl5759 <62679443+fndl5759@users.noreply.github.com>
2023-06-20 07:38:45 +0000

    Add GitHub Actions workflow

---
+++ test/WebApiTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,15 @@
+// add a namespace for the WeatherForecastController
+using WebApi.Controllers;
+
+namespace WebApiTests;
+
+[TestClass]
+public class WeatherForecastControllerTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // add a test method that tests the GetRange method of WeatherForecastContoller
+        
+    }
+}
